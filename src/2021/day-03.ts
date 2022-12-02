ead173e80f3873333441d2e5b93f37072521937b
Jean-Guy BUON <jean-guy.buon@aperam.com>
2022-12-02 05:51:05 +0100

    Activate format on save...

--- src/2021/day-03.ts
+++ src/2021/day-03.ts
@@ -6,9 +6,9 @@ export default async (input: string) => {
     for (let i = 0; i < positions[0].length; i++) {
         const bits = positions.map(p => p[i]);
         const mostCommonBit = bits.filter(b => b === '1').length > bits.filter(b => b === '0').length ? '1' : '0';
-        gamaRate+= mostCommonBit;
+        gamaRate += mostCommonBit;
         epsilonRate += mostCommonBit === '1' ? '0' : '1';
     }
-return [parseInt(gamaRate, 2) * parseInt(epsilonRate, 2), null];
+    return [parseInt(gamaRate, 2) * parseInt(epsilonRate, 2), null];
 }
 
