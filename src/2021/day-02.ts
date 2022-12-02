5c89073c2487e5fa02d61bbc4bb99ffccf5e6e4f
Jean-Guy BUON <jean-guy.buon@aperam.com>
2022-12-02 05:35:03 +0100

    2021 day 2

---
+++ src/2021/day-02.ts
@@ -0,0 +1,39 @@
+type direction = 'forward' | 'up' | 'down';
+
+export default async (input: string) => {
+    const instructions: [direction, number][] = input.split('\n').map(i => i.split(' ')).map(i => ([i[0] as any, +i[1]]));
+
+    const position1 = { horizontal: 0, depth: 0 };
+    instructions.forEach(([direction, value]) => {
+        switch (direction) {
+            case 'up':
+                position1.depth -= value;
+                break;
+            case 'down':
+                position1.depth += value;
+                break;
+            case 'forward':
+                position1.horizontal += value;
+                break;
+        }
+    })
+
+    const position2 = { horizontal: 0, depth: 0, aim: 0 };
+
+    instructions.forEach(([direction, value]) => {
+        switch (direction) {
+            case 'up':
+                position2.aim -= value;
+                break;
+            case 'down':
+                position2.aim += value;
+                break;
+            case 'forward':
+                position2.horizontal += value;
+                position2.depth += position2.aim * value;
+                break;
+        }
+    })
+
+    return [position1.depth * position1.horizontal, position2.depth * position2.horizontal];
+}
--- src/index.ts
+++ src/index.ts
@@ -9,31 +9,33 @@ import { IoHelper, FileHelper, PromiseHelper } from './helpers';
 
     const controlLocation = `./inputs/${year}/day-${day}/control`;
     await FileHelper.writeIfNotExists(controlLocation, '');
-    const controlInput = await FileHelper.read(controlLocation);
 
     const solutionLocation = `./inputs/${year}/day-${day}/solutions.json`;
     await FileHelper.writeIfNotExists(solutionLocation, '[null, null]');
-    const expectedSolutions = await FileHelper.read<number[]>(solutionLocation, 'json');
+
+    const inputLocation = `./inputs/${year}/day-${day}/input`;
+    await FileHelper.writeIfNotExists(inputLocation, '');
 
     const created = await FileHelper.writeIfNotExists(`./src/${year}/day-${day}.ts`, 'export default async (input: string) => {\nreturn [1, null];\n}\n\n');
     if (created) {
-        await PromiseHelper.wait(5);
+        await PromiseHelper.wait(2);
     }
+
     const action = await import(`./${year}/day-${day}`);
+    const controlInput = await FileHelper.read(controlLocation);
     const controlSolutions = await action.default(controlInput);
 
     if (!Array.isArray(controlSolutions) || controlSolutions.length !== 2) {
         throw new Error(`Incorrect response (array expected)!`);
     }
 
+    const expectedSolutions = await FileHelper.read<number[]>(solutionLocation, 'json');
     expectedSolutions.forEach((expectedSolution, index) => {
         if (expectedSolution !== controlSolutions[index]) {
             throw new Error(`Solution ${index+1} is wrong: expected = ${expectedSolution} / response = ${controlSolutions[index]}`);
         }
     })
 
-    const inputLocation = `./inputs/${year}/day-${day}/input`;
-    await FileHelper.writeIfNotExists(inputLocation, '');
     const input = await FileHelper.read(`./inputs/${year}/day-${day}/input`);
     const solutions: number[] = await action.default(input);
     solutions.forEach((solution, index) => {
