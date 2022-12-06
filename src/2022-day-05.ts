352449549f1c85a0cb6405938b83ff3c18e38300
Jean-Guy <jean-guy.buon@aperam.com>
2022-12-06 13:03:56 +0100

    2022 - day 5 (started)

---
+++ src/2022-day-05.ts
@@ -0,0 +1,14 @@
+
+import { InputParser } from './helpers';
+
+function part1(input: InputParser): string {
+    return null;
+}
+
+function part2(input: InputParser): string {
+    return null;
+}
+
+export default async (input: InputParser) => {
+    return [part1(input), part2(input)];
+}
